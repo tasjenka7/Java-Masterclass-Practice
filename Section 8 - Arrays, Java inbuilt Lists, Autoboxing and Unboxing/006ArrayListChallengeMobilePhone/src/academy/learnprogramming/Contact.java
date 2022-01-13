d7a9fbffc44446ae9eaa262c412105fac62957d4
tasjenka7 <tasjenka95@gmail.com>
2022-01-13 18:36:33 +0100

    ArrayList Challenge
    Creating a simple mobile phone functionalities

---
+++ Section 8 - Arrays, Java inbuilt Lists, Autoboxing and Unboxing/006ArrayListChallengeMobilePhone/src/academy/learnprogramming/Contact.java
@@ -0,0 +1,25 @@
+package academy.learnprogramming;
+
+public class Contact {
+
+    private String name;
+    private String phoneNumber;
+
+    public Contact(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    //static - we don't have to create new object
+    public static Contact createContact(String name, String phoneNumber){
+        return new Contact(name, phoneNumber);
+    }
+}
