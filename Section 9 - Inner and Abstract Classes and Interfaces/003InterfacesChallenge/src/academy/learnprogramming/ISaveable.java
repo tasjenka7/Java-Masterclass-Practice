14b56e7df466ec482b1f5c0cab0725d2ddcd7cb7
tasjenka7 <tasjenka95@gmail.com>
2022-02-02 22:54:52 +0100

    Interfaces Challenge

---
+++ Section 9 - Inner and Abstract Classes and Interfaces/003InterfacesChallenge/src/academy/learnprogramming/ISaveable.java
@@ -0,0 +1,10 @@
+package academy.learnprogramming;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface ISaveable {
+
+    List<String> write();
+    void read(List<String> savedValues);
+}
